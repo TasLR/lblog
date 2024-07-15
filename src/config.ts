2e869bde45e9ffe731ae82af084953ec0a8e5a49
TasLR <fri_lan@163.com>
2024-07-15 19:03:32 +0800

    first blog post

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
+  title: '暮沐笙箫',
+  subtitle: 'blog',
   lang: 'zh_CN', // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -44,8 +44,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: '暮沐笙箫',
+  bio: '非学无以广才,非志无以成学。',
   links: [
     {
       name: 'Twitter',
